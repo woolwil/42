2514dbe344f99c412c79c079a6f245735e63e47b
woolwil <wlwilcz@gmail.com>
2026-01-21 01:06:26 +0100

    awdwad

--- CoreNew/get_next_line/get_next_line.c
+++ CoreNew/get_next_line/get_next_line.c
@@ -17,7 +17,7 @@
 #include "get_next_line.h"
 
 char	read_to_stash(int fd, char *stash);
-char	extract_line(int fd, char *stash);
+char	extract_line(char *stash);
 
 char	*get_next_line(int fd)
 {
@@ -33,13 +33,13 @@ char	read_to_stash(int fd, char *stash)
 	char	*buff;
 	int		bytes_read;
 
-	bytes_read = 1;
 	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
 		{
 			free(stash);
 			return (NULL);
 		}
+	bytes_read = 1;
 	while (!ft_strchr(stash, '\n') && bytes_read != 0)
 	{
 		bytes_read = read(fd, buff, BUFFER_SIZE);
@@ -56,7 +56,35 @@ char	read_to_stash(int fd, char *stash)
 	return (stash);
 }
 
-char	extract_line(int fd, char *stash)
+char	extract_line(char *stash)
+{
+	size_t	i;
+	char	*line;
+
+	i = 0;
+	if (!stash[i])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i +2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] != '\n' && stash[i] != '\0')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{	
+		line[i] = '\n';
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*clean_stash(char *s1)
 {
 
 }
--- CoreNew/get_next_line/get_next_line_utils.c
+++ CoreNew/get_next_line/get_next_line_utils.c
@@ -28,31 +28,31 @@ size_t	ft_strlen(char *str)
 char	ft_strjoin(char *s1, char *s2)
 {
 	int		i;
-	int		j;
-	int		len;
-	char	*str;
+	size_t	len;
+	char	*buff;
 
 	i = 0;
-	j = 0;
-	if (!s2)
-		return (NULL);
 	len = ft_strlen(s2);
 	if (s1)
 		len += ft_strlen(s1);
-	str = malloc(sizeof(char) * (len + 1));
-	if (!str)
+	buff = malloc(sizeof(char) * (len + 1));
+	if (!buff)
+	{
+		free(s1);
 		return (NULL);
+	}
 	while (s1 && s1[i])
 	{
-		str[i] = s1[i];
+		buff[i] = s1[i];
 		i++;
 	}
-	while (s2[j])
-		str[i++] = s2[j++];
-	str[i] = '\0';
+	while (*s2)
+		buff[i++] = *s2++;
+	buff[i] = '\0';
 	free (s1);
-	return (str);
+	return (buff);
 }
+
 
 char	*ft_strchr(const char *s, int c)
 {
