008b82896c3505f7a665b07be4559dad28470d6d
ngvo <wlwilcz@gmail.com>
2026-01-25 17:30:44 +0100

    fixed norm errors within mains

--- CoreNew/get_next_line/main.c
+++ CoreNew/get_next_line/main.c
@@ -6,26 +6,29 @@
 /*   By: ngvo <ngvo@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 17:21:16 by ngvo              #+#    #+#             */
-/*   Updated: 2026/01/24 18:59:04 by ngvo             ###   ########.fr       */
+/*   Updated: 2026/01/25 17:29:13 by ngvo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 int	main(void)
 {
-	int	fd;
+	int		fd;
 	char	*line;
 
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
 		return (1);
-	while ((line = get_next_line(fd)))
+	line = get_next_line(fd);
+	while (line)
 	{
 		printf("%s", line);
-		free (line);
+		free(line);
+		line = get_next_line(fd);
 	}
-	close (fd);
+	close(fd);
 	return (0);
 }
--- CoreNew/get_next_line/main_bonus.c
+++ CoreNew/get_next_line/main_bonus.c
@@ -1,34 +1,63 @@
-#include "get_next_line_bonus.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngvo <ngvo@student.42prague.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25 17:30:00 by ngvo              #+#    #+#             */
+/*   Updated: 2026/01/25 17:28:50 by ngvo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <fcntl.h>
 
-int main(void)
+void	print_and_free(char *prefix, char *line)
 {
-    int fd1 = open("file1.txt", O_RDONLY);
-    int fd2 = open("file2.txt", O_RDONLY);
-    int fd3 = open("file3.txt", O_RDONLY);
-    char *line1, *line2, *line3;
-    int i;
+	if (line)
+	{
+		printf("%s%s", prefix, line);
+		free(line);
+	}
+}
 
-    if (fd1 == -1 || fd2 == -1 || fd3 == -1)
-    {
-        perror("open");
-        return (1);
-    }
+void	open_files(int *fd1, int *fd2, int *fd3)
+{
+	*fd1 = open("file1.txt", O_RDONLY);
+	*fd2 = open("file2.txt", O_RDONLY);
+	*fd3 = open("file3.txt", O_RDONLY);
+}
 
-    printf("--- Alternating reads from 3 files ---\n");
-    for (i = 0; i < 4; i++)
-    {
-        line1 = get_next_line(fd1);
-        if (line1) { printf("[file1] %s", line1); free(line1); }
-        line2 = get_next_line(fd2);
-        if (line2) { printf("[file2] %s", line2); free(line2); }
-        line3 = get_next_line(fd3);
-        if (line3) { printf("[file3] %s", line3); free(line3); }
-    }
+void	close_files(int fd1, int fd2, int fd3)
+{
+	close(fd1);
+	close(fd2);
+	close(fd3);
+}
 
-    close(fd1);
-    close(fd2);
-    close(fd3);
-    return 0;
+int	main(void)
+{
+	int	fd1;
+	int	fd2;
+	int	fd3;
+	int	i;
+
+	open_files(&fd1, &fd2, &fd3);
+	if (fd1 == -1 || fd2 == -1 || fd3 == -1)
+	{
+		perror("open");
+		return (1);
+	}
+	printf("--- Alternating reads from 3 files ---\n");
+	i = 0;
+	while (i < 4)
+	{
+		print_and_free("[file1] ", get_next_line(fd1));
+		print_and_free("[file2] ", get_next_line(fd2));
+		print_and_free("[file3] ", get_next_line(fd3));
+		i++;
+	}
+	close_files(fd1, fd2, fd3);
+	return (0);
 }
