a54101043c2836bf1645f8adf577a830b27a1dc2
namvoehome <wlwilcz@gmail.com>
2026-04-02 01:56:06 +0200

    pymod00 tweak

--- new_core/Milestone2/pymod00/all/ft_garden_name.py
+++
@@ -1,4 +0,0 @@
-def ft_garden_name():
-    name = input("Enter a garden name: ")
-    print("Garden: " + name)
-    print("Status: Growing well!")
--- new_core/Milestone2/pymod00/ex4/ft_plant_age.py
+++ new_core/Milestone2/pymod00/ex4/ft_plant_age.py
@@ -5,5 +5,6 @@ def ft_plant_age():
     else:
         print("Plant needs more time to grow.")
 
+
 if __name__ == "__main__":
     ft_plant_age()
