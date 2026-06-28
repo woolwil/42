0bd043517f8da3307cafe3a273d41fd8dfbbc9ca
ngvo <wlwilcz@gmail.com>
2026-06-28 16:35:39 +0200

    update

--- exam/rank02/level2/ft_range.c
+++ exam/rank02/level2/ft_range.c
@@ -4,25 +4,24 @@ int *ft_range(int start, int end)
 {
     int *arr;
     int size;
-    int i = 0;
+    int i;
+    int current;
+    int step;
 
-    size = abs((end - start)) + 1;
+    size = abs(end - start) + 1;
     arr = malloc(sizeof(int) * size);
-    if(start > end)
+
+    if(!arr)
+        return NULL;
+
+    i = 0;
+    current = start;
+    step = (start < end) ? 1 : -1;
+    while(i < size)
     {
-        while(start >= end)
-        {
-            arr[i] = start;
-            start -= 1;
-            i++;
-        }
+        arr[i] = current;
+        current += step;
+        ++i;
     }
-    else
-        while(start <= end)
-        {
-            arr[i] = start;
-            start += 1;
-            i++;
-        }
     return arr;
 }
--- exam/rank02/level2/ft_rrange.c
+++ exam/rank02/level2/ft_rrange.c
@@ -4,22 +4,24 @@ int *ft_rrange(int start, int end)
 {
     int *arr;
     int size;
-    int i = 0;
+    int i;
+    int current;
+    int step;
 
-    size = abs((end - start)) + 1;
+    size = abs(end - start) + 1;
     arr = malloc(sizeof(int) * size);
 
     if(!arr)
         return NULL;
         
+    i = 0;
+    current = end;
+    step = (start < end) ? -1 : 1;
     while(i < size)
     {
-        arr[i] = end;
-        if(start > end)
-            end++;
-        else
-            end--;
-        i++;
+        arr[i] = current;
+        current += step;
+        ++i;
     }
     return arr;
 }
