1f74511bea3bc09c2914363ee215c3f052cc6943
ngvo <wlwilcz@gmail.com>
2026-06-28 13:48:25 +0200

    w

--- exam/rank02/level2/expand_str.c
+++ exam/rank02/level2/expand_str.c
@@ -1,34 +1,34 @@
 #include <unistd.h>
 
-int wordlen(char *str)
+int word_len(char *s)
 {
     int len = 0;
-
-    while(str[len] && str[len] != ' ' && str[len] != '\t')
-        len++;
+    while(s[len] && s[len] != ' ' && s[len] != '\t')
+        ++len;
     return len;
 }
 
-void expand_str(char *str)
+void expand_str(char *s)
 {
     int first_word = 1;
     int len;
-    while(*str)
+
+    while(*s)
     {
-        while(*str == ' ' || *str == '\t')
-            ++str;
-        len = wordlen(str);
-        if(len > 0 && first_word == 0)
+        while(*s == ' ' || *s == '\t')
+            ++s;
+        len = word_len(s);
+        if(len > 0 && !first_word)
             write(1, "   ", 3);
         first_word = 0;
-        write(1, str, len);
-        str = str + len;
+        write(1, s, len);
+        s = s + len;
     }
 }
 
 int main(int ac, char **av)
 {
-    if (ac == 2)
+    if(ac == 2)
         expand_str(av[1]);
     write(1, "\n", 1);
 }
