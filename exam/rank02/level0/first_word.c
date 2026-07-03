9700aabc4735d2eee1674fc3cc88a5a12ce3ce8c
woolthink <wlwilcz@gmail.com>
2026-07-03 18:07:17 +0200

    w

--- exam/rank02/level0/first_word.c
+++ exam/rank02/level0/first_word.c
@@ -6,9 +6,7 @@ int	main(int ac, char **av)
 	if (ac == 2)
 	{
 		while (av[1][i] == ' ' && av[1][i] != '\t')
-		{
 			i++;
-		}
 		while (av[1][i] && av[1][i] != ' ')
 		{
 			write (1, &av[1][i], 1);
