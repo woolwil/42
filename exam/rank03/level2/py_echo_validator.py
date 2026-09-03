ead1298feee768946133d15c4a95e4d02077f653
ngvo <wlwilcz@gmail.com>
2026-09-03 19:07:40 +0200

    updated

--- exam/rank03/level2/py_echo_validator.py
+++ exam/rank03/level2/py_echo_validator.py
@@ -1,28 +1,15 @@
 def echo_validator(text: str) -> bool:
-    half = len(text) // 2
-    first_half = text[:half]
-    second_half = text[half:]
-    is_double_char = True
-    half_split = (text[:half] == text[half:])
-    if not text:
-        return True
-    if len(text) % 2 != 0:
-        return False
-    for i in range(0, len(text), 2):
-        if text[i] != text[i + 1]:
-            is_double_char = False
-            break
-    
-    return half_split or is_double_char
+    s = [c.lower() for c in text if c.isalpha()]
+    return s == s[::-1]
 
 
 def main() -> None:
-    print(echo_validator("abcabc"))      # True  (Half-split)
-    print(echo_validator("aabbcc"))      # True  (Double-char)
-    print(echo_validator("hellohello"))  # True  (Half-split)
-    print(echo_validator("abcd"))        # False (Neither)
-    print(echo_validator("racecar"))     # False (Odd length)
-    print(echo_validator(""))
+    print(echo_validator("racecar"))                            # True
+    print(echo_validator("Madam, I'm Adam"))                    # True
+    print(echo_validator("A man, a plan, a canal: Panama!"))    # True
+    print(echo_validator("Never odd or even"))                  # True
+    print(echo_validator("hello"))                              # False
+    print(echo_validator("abcde"))                              # False
 
 
 if __name__ == "__main__":
--- exam/rank03/level2/py_mirror_matrix.py
+++ exam/rank03/level2/py_mirror_matrix.py
@@ -1,14 +1,15 @@
 def mirror_matrix(matrix: list[list[int]]) -> list[list[int]]:
     return [row[::-1] for row in matrix]
 
-
 def main() -> None:
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-    print(mirror_matrix(matrix))
+    m1 = [[1, 2, 3],[4, 5, 6]]
+    m2 = [[1,2],[3,4],[5,6]]
+    m3 = [[-1,-2],[-3,-4]]
+    m4 = [[1,2,3,4]]
+    print(mirror_matrix(m1))
+    print(mirror_matrix(m2))
+    print(mirror_matrix(m3))
+    print(mirror_matrix(m4))
 
 
 if __name__ == "__main__":
