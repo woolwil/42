0cfaa95012477ebec6996b52ebda1e679102a2c4
woolthink <wlwilcz@gmail.com>
2026-09-11 15:17:25 +0200

    updated to match assignment

--- exam/rank03/level1/py_cryptic_sorter.py
+++ exam/rank03/level1/py_cryptic_sorter.py
@@ -1,5 +1,20 @@
+# def cryptic_sorter(strings: list[str]) -> list[str]:
+    # return sorted(strings, key=lambda s: (len(s), -sum(1 for char in s.lower() if char in "aeiou"), s.lower()))
+
 def cryptic_sorter(strings: list[str]) -> list[str]:
-    return sorted(strings, key=lambda s: (len(s), -sum(1 for char in s.lower() if char in "aeiou"), s.lower()))
+    def sort_key(s: str):
+        vowels = sum(1 for c in s if c in "aeiouAEIOU")
+        return (len(s), s.lower(), vowels)
+
+    res = list(strings)
+    legth = len(res)
+    for i in range(length):
+        for j in range(0, length - 1 - i):
+            if sort_key(res[j]) > sort_key(res[j + 1]):
+                res[j], res[j + 1] = res[j + 1], res[j]
+
+    return res
+
 
 def main() -> None:
     print(cryptic_sorter(["apple","cat","banana","dog","elephant"]))
